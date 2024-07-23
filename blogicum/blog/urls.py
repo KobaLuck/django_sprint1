ed9a316ed190b6e5bdd0c8a4282f0fab45612e9f
Nikita Kazantsev <kazantsev.neckit@yandex.ru>
2024-07-23 12:09:56 +0300

    Fixed PEP8

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 app_name = 'blog'
 
+url_cat = 'category/<slug:category_slug>/'
+
 urlpatterns = [
     path('posts/<int:id>/', views.post_detail, name='detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    path(url_cat, views.category_posts, name='category_posts'),
     path('', views.index, name='index')
 ]
