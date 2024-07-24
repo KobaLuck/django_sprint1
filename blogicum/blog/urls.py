3dcac20baddda743d28f3f149306710ea12644d5
Nikita Kazantsev <kazantsev.neckit@yandex.ru>
2024-07-24 12:58:22 +0300

    Fixed new bug

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = 'blog'
 
-url_cut = '<slug:category_slug>/'
+url_cut = 'category/<slug:category_slug>/'
 
 urlpatterns = [
-    path('<int:id>/', views.post_detail, name='detail'),
+    path('posts/<int:id>/', views.post_detail, name='detail'),
     path(url_cut, views.category_posts, name='category_posts'),
     path('', views.index, name='index')
 ]
