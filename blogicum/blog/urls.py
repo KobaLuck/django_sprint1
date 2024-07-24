021e0b1ed6b78f94af09e101beaad8ab2fd46767
Nikita Kazantsev <kazantsev.neckit@yandex.ru>
2024-07-24 12:45:30 +0300

    Excess commit

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -3,10 +3,15 @@ from . import views
 
 app_name = 'blog'
 
-url_cut = 'category/<slug:category_slug>/'
+url_cut = '<slug:category_slug>/'
 
 urlpatterns = [
-    path('posts/<int:id>/', views.post_detail, name='detail'),
+    path('<int:id>/', views.post_detail, name='detail'),
     path(url_cut, views.category_posts, name='category_posts'),
     path('', views.index, name='index')
+]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path(url_cut, views.category_posts, name='category_posts'),
 ]
