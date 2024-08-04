2b0b51bd77ab9b1366783100b9fcfc0c074571f8
Nikita Kazantsev <kazantsev.neckit@yandex.ru>
2024-08-04 14:23:08 +0300

    Changes in views

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
+from blog.views import category_posts, index, post_detail
 
 app_name = 'blog'
 
 url_cut = 'category/<slug:category_slug>/'
 
 urlpatterns = [
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
-    path(url_cut, views.category_posts, name='category_posts'),
-    path('', views.index, name='index')
+    path('posts/<int:id>/', post_detail, name='post_detail'),
+    path(url_cut, category_posts, name='category_posts'),
+    path('', index, name='index')
 ]
