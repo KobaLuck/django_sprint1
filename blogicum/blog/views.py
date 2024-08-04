019f678df75bd380f33a4b30e240d678772ea035
Nikita Kazantsev <kazantsev.neckit@yandex.ru>
2024-08-04 20:28:15 +0300

    again pep8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -52,7 +52,7 @@ def index(request):
 
 def post_detail(request, id):
     template = 'blog/detail.html'
-    context = {'post': posts[id]} 
+    context = {'post': posts[id]}
     return render(request, template, context)
 
 
